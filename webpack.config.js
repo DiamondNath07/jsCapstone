67128190a796d51cc489f1bde976b935fb37fa2d
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-12-10 11:24:37 +0100

    add fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  extends: 'lighthouse:default',
   mode: 'development',
   entry: './src/index.js',
   output: {
